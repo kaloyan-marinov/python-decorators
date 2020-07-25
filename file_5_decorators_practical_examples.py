83ebac757a46d4c4b6e3a9c0264861934b5d58c4
Kaloyan Marinov <kallo.marinov@gmail.com>
2020-07-25 12:21:54 +0300

    add `file_5_decorators_practical_examples.py`

---
+++ file_5_decorators_practical_examples.py
@@ -0,0 +1,58 @@
+# fmt: off
+'''
+Probably one of the more common use cases for decorators in Python is logging.
+'''
+# fmt: on
+
+
+def my_logger(orig_func):
+    """
+    This is a decorator.
+    It helps keep track of what argument the decorated function is run with.
+    """
+    import logging
+
+    logging.basicConfig(
+        filename="{}.log".format(orig_func.__name__), level=logging.INFO
+    )
+
+    def wrapper(*args, **kwargs):
+        logging.info("Ran with args: {}, and kwargs: {}".format(args, kwargs))
+        return orig_func(*args, **kwargs)
+
+    return wrapper
+
+
+def my_timer(orig_func):
+    """
+    This is a decorator.
+    It helps time how long a decoratored function takes to run.
+    """
+    import time
+
+    def wrapper(*args, **kwargs):
+        t1 = time.time()
+        result = orig_func(*args, **kwargs)
+        t2 = time.time() - t1
+        print("{} ran in: {} sec".format(orig_func.__name__, t2))
+        return result
+
+    return wrapper
+
+
+@my_logger
+def display_info(name, age):
+    print("display_info ran with arguments ({}, {})".format(name, age))
+
+
+import time
+
+
+@my_timer
+def display_info_slowly(name, age):
+    time.sleep(1)
+    print("display_info_slowly ran with arguments: ({}, {})".format(name, age))
+
+
+display_info("John", 25)  # This creates a `display_info.log` file.
+display_info_slowly("Jane", 30)
